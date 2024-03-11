289b73503bb68d933173228d0b6cabfdfa7e7e31
JoallyA <134223202+JoallyA@users.noreply.github.com>
2024-03-11 17:10:46 -0600

    Add files via upload

---
+++ redes__sociales__enriquecidas.js
@@ -0,0 +1,27 @@
+//ocultar-mostrar texto
+function muestra__oculta(id){
+    let div = document.getElementById(id);
+    if (div.style.display == "flex"){
+        div.style.display = "none";
+    }
+    else{
+        div.style.display = "flex";
+    }
+}
+
+// Mostrar u ocultar el botón de ir arriba dependiendo del desplazamiento
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollBtn").style.display = "none";
+  }
+}
+
+// Función para ir a la parte superior de la página
+function goToTop() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para otros navegadores
+}
